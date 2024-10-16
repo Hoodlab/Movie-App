47785157e05a8d6251635cf75883233573790785
hood <hoods.tech@gmail.com>
2024-10-16 19:25:15 +0300

    fix:json serialization exception, added default values for null items in ActorDto.kt

--- app/src/main/java/com/example/movieapp/actor_detail/data/remote/models/ActorDto.kt
+++ app/src/main/java/com/example/movieapp/actor_detail/data/remote/models/ActorDto.kt
@@ -7,31 +7,31 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class ActorDto(
     @SerialName("adult")
-    val adult: Boolean,
+    val adult: Boolean = false,
     @SerialName("also_known_as")
-    val alsoKnownAs: List<String>,
+    val alsoKnownAs: List<String> = emptyList(),
     @SerialName("biography")
-    val biography: String,
+    val biography: String = "",
     @SerialName("birthday")
-    val birthday: String,
+    val birthday: String = "",
     @SerialName("deathday")
     val deathday: String?,
     @SerialName("gender")
-    val gender: Int,
+    val gender: Int = 1,
     @SerialName("homepage")
     val homepage: String?,
     @SerialName("id")
     val id: Int,
     @SerialName("imdb_id")
-    val imdbId: String,
+    val imdbId: String = "",
     @SerialName("known_for_department")
-    val knownForDepartment: String,
+    val knownForDepartment: String = "",
     @SerialName("name")
-    val name: String,
+    val name: String = "",
     @SerialName("place_of_birth")
-    val placeOfBirth: String,
+    val placeOfBirth: String = "",
     @SerialName("popularity")
-    val popularity: Double,
+    val popularity: Double = 0.0,
     @SerialName("profile_path")
-    val profilePath: String
+    val profilePath: String = ""
 )
